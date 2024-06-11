25283bdede413428c4f1a558fad6a60e57661253
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-06-11 14:27:42 +0200

    fullscreen method

---
+++ js/fullscreen.js
@@ -0,0 +1,38 @@
+function openFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) { // Firefox
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { // IE/Edge
+        element.msRequestFullscreen();
+    }
+}
+
+// Funktion um Fullscreen zu verlassen
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { // Firefox
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+        document.msExitFullscreen();
+    }
+}
+
+// Funktion um den Fullscreen Modus zu toggeln
+function toggleFullscreen(element) {
+    if (!document.fullscreenElement &&    // alternative Standardmethode
+        !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // aktuelle Methoden
+        openFullscreen(element);
+    } else {
+        closeFullscreen();
+    }
+}
+
+document.getElementById('myCanvas').addEventListener('click', function() {
+    toggleFullscreen(this);
+});
