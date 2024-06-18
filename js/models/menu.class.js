5ffc6bf48d2e0703b126466d3a8cc8f7052250ff
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-06-18 13:45:57 +0200

    Menu drawin srtucture

---
+++ js/models/menu.class.js
@@ -0,0 +1,12 @@
+class Menu{
+
+    startButton = {
+        x: 300,
+        y: 250,
+        width: 200,
+        height: 100,
+        text: 'Start Game'
+    };
+
+
+}
--- js/models/menustate.class.js
+++ js/models/menustate.class.js
@@ -1,4 +1,14 @@
 class MenuState extends State {
+
+    menu = new Menu();
+
+
+
+
+
+
+
+
     enter() {
         console.log("Entering Menu State");
         // Hier kannst du Initialisierungslogik für das Menü hinzufügen
@@ -11,13 +21,29 @@ class MenuState extends State {
 
     draw() {
         const ctx = this.game.context;
+        
+        //this.menu.draw(ctx, menuBackground);
+        //this.menu.draw(ctx, title);
+        //this.menu.draw(ctx, startButton);
+        //this.menu.draw(ctx, creditsButton);
+        
+        
         ctx.clearRect(0, 0, this.game.canvas.width, this.game.canvas.height);
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, this.game.canvas.width, this.game.canvas.height);
         
+        
         ctx.fillStyle = 'white';
         ctx.font = '30px Arial';
         ctx.fillText('Hauptmenü', 100, 100);
+
+        
+        ctx.textAlign='center';
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.menu.startButton.x, this.menu.startButton.y, this.menu.startButton.width, this.menu.startButton.height);
+        ctx.fillStyle = 'black';
+        ctx.fillText(this.menu.startButton.text, this.menu.startButton.x + this.menu.startButton.width / 2, this.menu.startButton.y + this.menu.startButton.height / 2 + 10);
+
 
         // Weitere Menüelemente zeichnen
     }
